9727175af05f3c1e0d88bde59bbde88c0e1ceb38
Humayra Hussain <99496933+hhussain4@users.noreply.github.com>
2024-04-13 20:52:17 -0400

    Generating tab content

--- profilepage/lib/profilePage.dart
+++ profilepage/lib/profilePage.dart
@@ -1,4 +1,6 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:profilepage/settings.dart';
 
 class UserProfile extends StatefulWidget {
@@ -17,13 +19,95 @@ class _UserProfileState extends State<UserProfile> {
       _counter++;
     });
   } */
+  int selectedTabIndex =
+      0; //used to control which tab is being shown. By index.
 
   int getPostNum() => 42; // Dummy value
   int getFollowersNum() => 180; // Dummy value
   int getFollowingNum() => 560; // Dummy value
 
+  Widget _postsTabContent() {
+    return GridView.count(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 3,
+      crossAxisSpacing: 4,
+      mainAxisSpacing: 4,
+      children: List.generate(6, (index) {
+        if (index % 2 == 0) {
+          // Even indices: Display Images
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('https://via.placeholder.com/150'),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          );
+        } else {
+          // Odd indices: Display Text
+          return Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Item $index',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          );
+        }
+      }),
+    );
+  }
+
+  Widget _likedTabContent() {
+    return GridView.count(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 3,
+      crossAxisSpacing: 4,
+      mainAxisSpacing: 4,
+      children: List.generate(1, (index) {
+        if (index % 2 == 0) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('https://via.placeholder.com/150'),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          );
+        } else {
+          return Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Liked $index',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          );
+        }
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    //below list is for the content of posts and liked posts tabs
+    List<Widget> tabsContent = [
+      _postsTabContent(),
+      _likedTabContent(),
+    ];
+    //
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -36,7 +120,7 @@ class _UserProfileState extends State<UserProfile> {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const SettingsPage()),
+                MaterialPageRoute(builder: (context) => const Settings()),
               );
             },
           ),
@@ -48,42 +132,53 @@ class _UserProfileState extends State<UserProfile> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
-                children: <Widget>[
-                  // Circle profile image
-                  ClipOval(
-                    child: Image.network(
-                      'https://via.placeholder.com/150', // Placeholder image URL
-                      width: MediaQuery.of(context).size.width *
-                          0.2, // 30% of screen width
-                      height: MediaQuery.of(context).size.width *
-                          0.2, // Same as width to keep it a circle
-                      fit: BoxFit.cover, // Cover bounds of the widget
-                    ),
-                  ),
-                  const SizedBox(width: 10), // Space between image & textbox
-                  // Textbox
-                  // ignore: prefer_const_constructors
-                  Expanded(
+              // ignore: prefer_const_constructors
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: <Widget>[
+                    // Circle profile image taking up 30% of the screen width
                     // ignore: prefer_const_constructors
-                    child: TextField(
+                    CircleAvatar(
                       // ignore: prefer_const_constructors
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Bio Info Goes Here',
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 8.5, horizontal: 10.0),
-                      ),
-                      // Adjusts height to match the image
-                      // ignore: prefer_const_constructors
-                      style: TextStyle(
-                        fontSize: 16, //smaller so it doesn't look too big.
-                      ),
-                      maxLines: 2, //only want to so two rows of text!
+                      backgroundImage: NetworkImage(
+                          'https://static-00.iconduck.com/assets.00/profile-circle-icon-512x512-zxne30hp.png'),
+                      radius: 40,
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                ],
+                    SizedBox(
+                        width:
+                            20), // Provides space between the image and the text box
+
+                    // Expanded TextField on the right with "Bio Info" label text
+                    Expanded(
+                      child: SizedBox(
+                        height: 50, // Fixed height for better control
+                        child: Theme(
+                          data: Theme.of(context).copyWith(
+                            colorScheme: Theme.of(context).colorScheme.copyWith(
+                                  primary: Colors.black,
+                                ),
+                          ),
+                          // ignore: prefer_const_constructors
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Bio Info',
+                              // Try reducing or removing padding to see if it affects visibility
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 12.0, horizontal: 10.0),
+                            ),
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black), // Ensure black text
+                            maxLines: null, // Flexible vertical expansion
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               const SizedBox(height: 11),
               //row below for the posts, following, and followers numbers.
@@ -119,6 +214,46 @@ class _UserProfileState extends State<UserProfile> {
                   ),
                 ],
               ),
+              const SizedBox(height: 20),
+              //below this comment is the Tabs content for this page
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  //gesture detector for user posts
+                  GestureDetector(
+                    onTap: () => setState(() => selectedTabIndex = 0),
+                    child: Text(
+                      'Posts',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: selectedTabIndex == 0
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        color:
+                            selectedTabIndex == 0 ? Colors.blue : Colors.black,
+                      ),
+                    ),
+                  ),
+                  //gesture detector for liked
+                  GestureDetector(
+                    onTap: () => setState(() => selectedTabIndex = 1),
+                    child: Text(
+                      'Liked',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: selectedTabIndex == 1
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        color:
+                            selectedTabIndex == 1 ? Colors.green : Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              //tab content below!
+              tabsContent[selectedTabIndex],
             ],
           ),
         ),
